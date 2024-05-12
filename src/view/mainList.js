750d8c11bb22b343831439e5d8aa38ddefcda804
unknown <ekfzma1004@gmail.com>
2024-05-12 22:56:09 +0900

    style: list, detail view 스타일 추가

--- src/view/mainList.js
+++ src/view/mainList.js
@@ -13,24 +13,12 @@ class ListItem extends HTMLElement {
     this.start();
   }
 
-  start() {
-    this.innerHTML = `
-      <style>
-        ul{
-          list-style:none;
-        }
-        .list-image {
-          width: 200px;
-          height: 200px;
-        }
-      </style>
-      `;
-    
+  start() {    
     const category = this.state.listItem[0].category_title === "개발" ? "개발" : "디자인";
 
     this.innerHTML += 
-        `<div>
-          <h1>${category}</h1>
+        `<section class='px-[30px] max-w-[980px] mb-[192px] mx-auto'>
+          <h1 class="py-[20px] text-[36px] font-[700] mt-[52px] mb-[32px]">${category}</h1>
           ${this.state?.listItem
             .map(
               ({
@@ -40,20 +28,20 @@ class ListItem extends HTMLElement {
                 description,
                 uploadDate,
               }) =>
-                `<ul>
-                    <li data-navigate="/detail/${id}">
-                      <div>
-                        <img class="list-image" src=${thumbImg} />
-                        <h1>${title}</h1>
-                        <p>${description}</p>
-                        <time>${uploadDate}</time>
+                `<li data-navigate="/detail/${id}" class="cursor-pointer mb-[85px] last:mb-[0] list-none">
+                      <div class="flex items-center gap-x-[48px]">
+                        <img class=" flex-shrink-0 flex-grow-0 flex-[240px] w-[240px] h-[240px] overflow-hidden object-cover rounded-[14px]" src=${thumbImg} />
+                        <div>
+                          <h1 class="mb-[14px] text-[#333d4b] font-[700] text-[34px]">${title}</h1>
+                          <p class="mb-[12px] text-[#4e5968] text-ellipsis line-clamp-2 leading-[1.6]">${description}</p>
+                          <time class="text-[15px] text-[#8b95a1] leading-[1.6]">${uploadDate}</time>
+                        </div>
                       </div>
-                    </li>
-                  </ul>
+                  </li>
                 `
             )
           .join("")}
-        </div>`;
+        </section>`;
     }
   }
   
